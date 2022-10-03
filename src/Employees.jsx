68f699f56cbdb13f9e84c91203d240a392db2391
Mojahidul Islam <78002811+Nightstalkeer@users.noreply.github.com>
2022-10-03 18:43:42 +0000

    Node modules deleted

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -1,118 +1,14 @@
-import { useState } from 'react'; // use state is hook
 import femaleProfile from './images/femaleProfile.jpg';
 import maleProfile from './images/maleProfile.jpg';
 
 
-const Employees = () => {
+const Employees = (props/*{employees, selectedTeam, handleEmployeeCardClick, handleTeamSelectionChange}*/) => {
 
-  const [selectedTeam, setTeam] = useState("TeamB");
+  
+  // We can do both by destructuring or by props and after that props dot operator.....
 
-  const [employees, setEmployees] = useState([{
-    id: 1,
-    fullName: "Bob Jones",
-    designation: "JavaScript Developer",
-    gender: "male",
-    teamName: "TeamA"
-  },
-  {
-    id: 2,
-    fullName: "Jill Bailey",
-    designation: "Node Developer",
-    gender: "female",
-    teamName: "TeamA"
-  },
-  {
-    id: 3,
-    fullName: "Gail Shepherd",
-    designation: "Java Developer",
-    gender: "female",
-    teamName: "TeamA"
-  },
-  {
-    id: 4,
-    fullName: "Sam Reynolds",
-    designation: "React Developer",
-    gender: "male",
-    teamName: "TeamB"
-  },
-  {
-    id: 5,
-    fullName: "David Henry",
-    designation: "DotNet Developer",
-    gender: "male",
-    teamName: "TeamB"
-  },
-  {
-    id: 6,
-    fullName: "Sarah Blake",
-    designation: "SQL Server DBA",
-    gender: "female",
-    teamName: "TeamB"
-  },
-  {
-    id: 7,
-    fullName: "James Bennet",
-    designation: "Angular Developer",
-    gender: "male",
-    teamName: "TeamC"
-  },
-  {
-    id: 8,
-    fullName: "Jessica Faye",
-    designation: "API Developer",
-    gender: "female",
-    teamName: "TeamC"
-  },
-  {
-    id: 9,
-    fullName: "Lita Stone",
-    designation: "C++ Developer",
-    gender: "female",
-    teamName: "TeamC"
-  },
-  {
-    id: 10,
-    fullName: "Daniel Young",
-    designation: "Python Developer",
-    gender: "male",
-    teamName: "TeamD"
-  },
-  {
-    id: 11,
-    fullName: "Adrian Jacobs",
-    designation: "Vue Developer",
-    gender: "male",
-    teamName: "TeamD"
-  },
-  {
-    id: 12,
-    fullName: "Devin Monroe",
-    designation: "Graphic Designer",
-    gender: "male",
-    teamName: "TeamD"
-  }]);
-
-  //Functions are delared here
-
-  function handleTeamSelectionChange(event) {
-    let s = event.target.value;
-    // console.log(typeof (event.target.value));
-    setTeam(s);
-  }
-
-
-  console.log(selectedTeam);
-
-
-  function handleEmployeeCardClick(event) {
-    const transformedEmployees = employees.map((employee) => employee.id === parseInt(event.currentTarget.id) ? (employee.teamName === selectedTeam) ? { ...employee, teamName: '' } : { ...employee, teamName: selectedTeam } : employee);
-
-    setEmployees(transformedEmployees);
-
-    // console.log(transformedEmployees);
-    // console.log(selectedTeam);
-    // console.log(String(event.currentTarget.id));
-  }
+  
+  const employees = props.employees, selectedTeam= props.selectedTeam, handleEmployeeCardClick = props.handleEmployeeCardClick, handleTeamSelectionChange = props.handleTeamSelectionChange;
 
   return (
     <main className="container">
